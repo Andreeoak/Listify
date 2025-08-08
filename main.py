d95edb147c40a5a44fa71d098e76c38e08815d37
AndreeOak <andreesoul@gmail.com>
2025-08-08 15:09:34 -0300

    Read task by id endpoint

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
+from fastapi.responses import RedirectResponse
 from sqlalchemy.orm import Session
 from typing import Annotated
 from Database.Models.ToDosModel import ToDosModel
-from Database.database import engine, SessionLocal, Base, getDb
+from Database.database import engine, Base, getDb
 import os
 
 """
@@ -15,5 +16,16 @@ Base.metadata.create_all(bind=engine)
 db_dependency = Annotated[Session, Depends(getDb)]
 
 @app.get("/")
+async def redirect_to_tasks():
+    return RedirectResponse(url="/Tasks")
+
+@app.get("/Tasks")
 async def readAllEntries(db: db_dependency):
     return db.query(ToDosModel).all()
+
+@app.get("/Tasks/{task_id}")
+async def readTaskById(db:db_dependency, task_id:int):
+    model =db.query(ToDosModel).filter(ToDosModel.id ==task_id).first()
+    if(model is not None):
+        return model
+    raise HTTPException(status_code=404, detail="Task not found")
