4306339c4f443cb8279db9bd8be5ffa77854e12c
AndreeOak <andreesoul@gmail.com>
2025-08-08 14:59:18 -0300

    Read all entries datapoint

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from typing import Annotated
+from Database.Models.ToDosModel import ToDosModel
+from Database.database import engine, SessionLocal, Base, getDb
+import os
 
-import Database.Models.ToDosModel as Models
-from Database.database import engine
+"""
+print("Engine URL:", engine.url)
+print("Banco usado:", os.path.abspath("todos.db"))
+"""
 
 app = FastAPI()
+Base.metadata.create_all(bind=engine)
+db_dependency = Annotated[Session, Depends(getDb)]
 
-Models.Base.metadata.create_all(bind=engine)
+@app.get("/")
+async def readAllEntries(db: db_dependency):
+    return db.query(ToDosModel).all()
