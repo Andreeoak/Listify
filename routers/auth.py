9a88968017c1e9fad9632f0d8d3de2dfdca2fe73
AndreeOak <andreesoul@gmail.com>
2025-08-11 19:06:18 -0300

    verify password with Oauth2

--- routers/auth.py
+++ routers/auth.py
@@ -5,9 +5,19 @@ from Utils.encryption import EncryptionContext
 from Database.database import getDb
 from sqlalchemy.orm import Session
 from typing import Annotated
+from fastapi.security import OAuth2PasswordRequestForm
 
 router = APIRouter()
 db_dependency = Annotated[Session, Depends(getDb)]
+
+def authenticate_user(username:str, password:str, db:db_dependency):
+    user = db.query(UsersModel).filter(UsersModel.username==username).first()
+    if(not user):
+        return False
+    if(not EncryptionContext.verifyPassword(password, user.hashed_password)):
+        return False
+    return True
+
 
 @router.post("/auth/", status_code=status.HTTP_201_CREATED)
 async def createUser(db:db_dependency, createUserRequest: UserInterface):
@@ -27,3 +37,12 @@ async def createUser(db:db_dependency, createUserRequest: UserInterface):
         "Message": "User created succesfully!",
         "User":   create_user_model 
     }
+    
+@router.post("/token")
+async def loginForAccessToken(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+    db: db_dependency):
+    user = authenticate_user(form_data.username, form_data.password, db)
+    if(not user):
+        return "Failed Authentication"
+    return "Authentication Successful!"
