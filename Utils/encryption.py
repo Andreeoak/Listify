82fd6ed4fe91bd902e668e308501a2e5e6b76ec8
AndreeOak <andreesoul@gmail.com>
2025-08-12 20:02:45 -0300

    jwt decode method to verify the current usert

--- Utils/encryption.py
+++ Utils/encryption.py
@@ -1,8 +1,11 @@
 from passlib.context import CryptContext
 #Passlib 1.7.4 + bcrypt 4.1.2 (💥 breaks)
 #Passlib 1.7.4 + bcrypt 4.0.1 (✅ works — sweet spot)
-from jose import jwt
+from jose import jwt, JWSError
 from datetime import timedelta, datetime, timezone
+from fastapi.security import OAuth2PasswordBearer
+from typing import Annotated
+from fastapi import Depends, HTTPException, status
 
 class EncryptionContext:
     # Create only once at import time
@@ -19,6 +22,7 @@ class EncryptionContext:
 class jwtEncryption:
     SECRET_KEY = '69905614fb3c9e2b433ae26e9e198660da6d9daedbe8a9e63669aae008db7095'
     ALGORITHM = 'HS256'
+    oauth2_bearer = OAuth2PasswordBearer(tokenUrl="token")
     
     @classmethod
     def createAccessToken(cls, username:str, user_id:int, exp: timedelta):
@@ -26,3 +30,15 @@ class jwtEncryption:
         expires = datetime.now(timezone.utc) + exp
         encode.update({'exp':expires})
         return jwt.encode(encode, cls.SECRET_KEY, cls.ALGORITHM)
+    
+    @classmethod
+    async def getCurrentUser(cls, token: Annotated[str, Depends(oauth2_bearer)]):
+        try:
+            payload = jwt.decode(token, cls.SECRET_KEY, algorithms=[cls.ALGORITHM])
+            username:str = payload.get('sub')
+            user_id: int = payload.get('id')
+            if username is None or user_id is None:
+                raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not verify credentials")
+            return {"username":username, "id":user_id}
+        except JWSError:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not verify credentials")
