b9409736dbe46c84009573d40e9d7d4db94e9cb9
AndreeOak <andreesoul@gmail.com>
2025-08-16 15:01:17 -0300

    Creating route call that allows users to change their password

--- routers/users.py
+++ routers/users.py
@@ -3,6 +3,7 @@ from Database.database import getDb
 from sqlalchemy.orm import Session
 from typing import Annotated
 from Database.Models.UsersModel import UsersModel
+from Interfaces.UserVerificationInterface import UserVerificationInterface as UserVerification
 from Utils.encryption import jwtEncryption
 from Utils.encryption import EncryptionContext
 
@@ -21,3 +22,14 @@ async def getUser(user:user_dependency, db:db_dependency):
         raise HTTPException(status_code=401, detail='Authentication Failed!')
     return db.query(UsersModel).filter(UsersModel.id == user.get("id")).first()
 
+@router.put("/password", status_code=status.HTTP_204_NO_CONTENT)
+async def changeUserPassword(user:user_dependency, db:db_dependency, user_verification:UserVerification):
+    if user is None:
+        raise HTTPException(status_code=401, detail='Authentication Failed!')
+    user_model = db.query(UsersModel).filter(UsersModel.id == user.get('id')).first()
+    
+    if not EncryptionContext.verifyPassword(user_verification.password, user_model.hashed_password):
+        raise HTTPException(status_code=401, detail='Could not confirm password.')
+    user_model.hashed_password = EncryptionContext.hashPassword(user_verification.new_password)
+    db.add(user_model)
+    db.commit()
