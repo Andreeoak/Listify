88be35faa5e8246f80291d5ef422f1b3ec37f4c8
AndreeOak <andreesoul@gmail.com>
2025-08-18 19:38:52 -0300

    Moack database for integration test

--- Test/test_todos.py
+++ Test/test_todos.py
@@ -1,5 +1,25 @@
 from main import app
-from Database.database import getDb
 from Database.MockDatabase import override_getDb
+from routers.toDos import getDb
+from Utils.encryption import jwtEncryption
+from fastapi.testclient import TestClient
+from fastapi import status
+
+def override_get_current_user():
+    return {
+        "username": "andreCarvalho",
+        "id": 1,
+        "user_role": "admin"
+    }
+
 
 app.dependency_overrides[getDb] = override_getDb
+app.dependency_overrides[jwtEncryption.getCurrentUser] = override_get_current_user
+
+Client = TestClient(app)
+
+def testReadAllAuthenticated():
+    response = Client.get("/")
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == []
+    
