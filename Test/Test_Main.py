1a7c08e31a8e6c9ac814eeaa03c861e3468b652b
AndreeOak <andreesoul@gmail.com>
2025-08-18 18:23:36 -0300

    Basic test for main health check

---
+++ Test/Test_Main.py
@@ -0,0 +1,14 @@
+from fastapi.testclient import TestClient
+from fastapi import status
+import main
+import pytest
+
+client = TestClient(main.app)
+
+def testHealthCheckHTTPResponse():
+    response = client.get("/health")
+    assert response.status_code == status.HTTP_200_OK, "Normal response is HTTP 200 - OK"
+    
+def testHealthCheckJsonResponse():
+    response = client.get("/health")
+    assert response.json() == {'status': 'Healthy'}, "Expected response is that the server is Healthy"
--- main.py
+++ main.py
@@ -7,6 +7,13 @@ from Database.Models.ToDosModel import ToDosModel
 
 app = FastAPI()
 Base.metadata.create_all(bind=engine) 
+
+@app.get("/health")
+def healthCheck():
+    return{
+        'status': 'Healthy'
+    }
+    
 app.include_router(auth.router)
 app.include_router(toDos.router)
 app.include_router(admin.router)
