81d2c0a50795a861d8a813ca13bff11155ad3e52
AndreeOak <andreesoul@gmail.com>
2025-08-19 11:08:50 -0300

    Mocking testReadAllAuthenticated with fixture

--- Test/test_todos.py
+++ Test/test_todos.py
@@ -1,11 +1,14 @@
+from sqlalchemy import text
 from main import app
-from Database.MockDatabase import override_getDb
+from Database.MockDatabase import override_getDb, TestingSessionLocal, engine
 from routers.toDos import getDb
 from Utils.encryption import jwtEncryption
 from fastapi.testclient import TestClient
 from fastapi import status
+import pytest
+from Database.Models.ToDosModel import ToDosModel
 
-def override_get_current_user():
+async def override_get_current_user():
     return {
         "username": "andreCarvalho",
         "id": 1,
@@ -18,8 +21,34 @@ app.dependency_overrides[jwtEncryption.getCurrentUser] = override_get_current_us
 
 Client = TestClient(app)
 
-def testReadAllAuthenticated():
-    response = Client.get("/")
+@pytest.fixture
+def testTodo():
+    todo = ToDosModel(
+        title= "Learn to code!",
+        description= "Need to learn everyday!",
+        priority=5,
+        complete=False,
+        owner_id=1
+    )
+    db = TestingSessionLocal()
+    db.add(todo)
+    db.commit()
+    yield todo
+    with engine.connect() as connection:
+        connection.execute(text("Delete from todos;"))
+        connection.commit()
+
+def testReadAllAuthenticated(testTodo):
+    response = Client.get("/Tasks")
     assert response.status_code == status.HTTP_200_OK
-    assert response.json() == []
+    assert response.json() == [
+        {
+            "complete": False,
+            "title": "Learn to code!",
+            "description": "Need to learn everyday!",
+            "id":1,
+            "priority": 5,
+            "owner_id":1
+        }
+    ]
     
