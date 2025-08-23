cd2f6f472c753172bb5db835a8202f39373475f5
AndreeOak <andreesoul@gmail.com>
2025-08-23 18:12:54 -0300

    Testing async function

--- Test/test_auth.py
+++ Test/test_auth.py
@@ -5,44 +5,6 @@ from fastapi import status
 from datetime import timedelta
 from jose import jwt
 
-"""
-router = APIRouter(
-prefix='/auth',
-tags=['Auth']
-)
-db_dependency = Annotated[Session, Depends(getDb)]
-
-
-@router.post("/", status_code=status.HTTP_201_CREATED)
-async def createUser(db:db_dependency, createUserRequest: UserInterface):
-create_user_model = UsersModel(
-    email = createUserRequest.email,
-    username = createUserRequest.username,
-    first_name = createUserRequest.first_name,
-    last_name= createUserRequest.last_name,
-    role = createUserRequest.role,
-    phone_number = createUserRequest.phone_number,
-    hashed_password = EncryptionContext.hashPassword(createUserRequest.password),
-    is_active = True
-)
-db.add(create_user_model)
-db.commit()
-db.refresh(create_user_model)
-return{ 
-    "Message": "User created succesfully!",
-    "User":   create_user_model 
-}
-
-@router.post("/token", response_model=TokenInterface)
-async def loginForAccessToken(
-form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
-db: db_dependency):
-user = authenticate_user(form_data.username, form_data.password, db)
-if(not user):
-    raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not verify credentials")
-token = jwtEncryption.createAccessToken(user.username, user.id, user.role, timedelta(minutes=20))
-return {'access_token':token, "token_type": 'bearer'}
-"""
 
 client = getTestClient()
 
@@ -71,3 +33,15 @@ def testCreateAccessToken():
     assert decoded_token['sub'] == username
     assert decoded_token['id']  == user_id
     assert decoded_token['role']  == role
+
+@pytest.mark.asyncio
+async def testGetCurrentUser():
+    username = 'testUser'
+    user_id = 4
+    role = 'admin'
+    encode = {'sub': username, "id": user_id, 'role':role}
+    token = jwt.encode(encode, jwtEncryption.SECRET_KEY, jwtEncryption.ALGORITHM)
+    
+    user = await jwtEncryption.getCurrentUser(token)
+    
+    assert user == {"username": username, "id": user_id, "user_role": role}
