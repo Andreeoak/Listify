ecc4c973568edda4346c6ab8f71b5977a8e119aa
AndreeOak <andreesoul@gmail.com>
2025-08-23 21:41:03 -0300

    Intalling Jinja and creating Template folder for website!

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
 from routers import auth, toDos, admin, users
 from Database.database import engine, Base
 # Importar modelos antes de criar tabelas
@@ -7,6 +8,11 @@ from Database.Models.ToDosModel import ToDosModel
 
 app = FastAPI()
 Base.metadata.create_all(bind=engine) 
+templates = Jinja2Templates(directory="Templates")
+
+@app.get("/")
+def test(request: Request):
+    return templates.TemplateResponse("home.html", {"request" : request})
 
 @app.get("/health")
 def healthCheck():
